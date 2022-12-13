42adff512f6c731542abc2f0a01abc7126511376
selimelsayed20 <117734525+selimelsayed20@users.noreply.github.com>
2022-12-13 13:23:38 +0200

    selim

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -15,17 +15,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		putchar(%d '+' ,n);
+		putchar('+');
 		return (1);
 	}
 	else if (n == 0)
 	{
-		putchar(%d '0' ,n);
+		putchar('0');
 		return (0);
 	}
 	else
 	{
-		putchar(%d '-' ,n);
+		putchar('-');
 		return (-1);
 	}
 }
