ae9f678521641382eb27cd4d08646038d3a0743c
selimelsayed20 <117734525+selimelsayed20@users.noreply.github.com>
2022-12-13 13:00:49 +0200

    selim

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * _islower - Shows 1 if the input is a
+ * lowercase character. Another cases, shows
+ * 0
+ *
+ * @c: The character in ASCII code
+ *
+ * Return: 1 for lowercase character. 0 for the rest.
+ */
+int _isalpha(int c)
+{
+	if ((islower(c)) && (isalpha(c)) )
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
