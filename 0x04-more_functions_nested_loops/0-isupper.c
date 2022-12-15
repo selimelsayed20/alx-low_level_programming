5e0d7c89c8e7190fac586dff1f1cfc8f9da60600
Selim Elsayed <saleemelsayed17@gmail.com>
2022-12-15 22:40:06 +0200

    selim

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - checks for uppercase character.
+ *.
+ * Return: Returns 1 if c is uppercase or Returns 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A')
+	{
+		return (1);
+	}
+	else
+	return (0);
+}
