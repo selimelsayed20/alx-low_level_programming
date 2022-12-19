83e90485609e2def66149fc0f33c5e2de05971f1
Selim Elsayed <saleemelsayed17@gmail.com>
2022-12-19 11:39:50 +0200

    selim

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,10 +1,15 @@
 #include "main.h"
-#include "_putchar.c"
-#include "2-main.c"
-
 
 /**
- * reset_to_98 - takes a pointer to a parameter and updates its value.
+ * _strlen - a function that swaps the values of two integers.
  * @s: input string.
  * Return: no return.
  */
+
+int _strlen(char *s)
+{
+	int i = 0;
+	while (*(s +i) != '\0')
+		i++;
+		return(i);	
+}
