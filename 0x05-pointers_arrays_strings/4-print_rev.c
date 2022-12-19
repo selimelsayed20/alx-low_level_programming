7c5a152cc03ae4eb434094cd5891f2e5639414a7
Selim Elsayed <saleemelsayed17@gmail.com>
2022-12-19 14:05:25 +0200

    selim

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,10 +1,22 @@
 #include "main.h"
-#include "_putchar.c"
-#include "4-main.c"
-
 
 /**
  * reset_to_98 - takes a pointer to a parameter and updates its value.
  * @s: input string.
  * Return: no return.
  */
+void print_rev(char *s)
+{
+	int x = 0;
+
+	while (x >= 0)
+	{
+		if (s[x] == '\0')
+			break;
+		x++;
+	}
+
+	for (x--; x >= 0; x--)
+		_putchar(s[x]);
+	_putchar('\n');
+}
