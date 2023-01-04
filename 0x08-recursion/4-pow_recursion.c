a733bfe1c532bf7c29abb65902961f62399fa96f
Selim Elsayed <saleemelsayed17@gmail.com>
2023-01-04 11:10:04 +0200

    selim

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,3 +1,24 @@
 #include "main.h"
-#include "_putchar.c"
-#include "4-main.c"
+
+/**
+ * _pow_recursion - a function that returns the value
+ * @x: int
+ * @y: int
+ * Return:(0).
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
