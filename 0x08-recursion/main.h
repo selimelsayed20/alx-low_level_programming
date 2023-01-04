83cec87c31ecd2455ff268c5742998b062fe8496
Selim Elsayed <saleemelsayed17@gmail.com>
2023-01-04 09:21:21 +0200

    selim

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,5 +1,3 @@
-#ifndef FILE_HOLBERTON
-#define FILE_HOLBERTON
 
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
@@ -10,5 +8,3 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
-
-#endif
