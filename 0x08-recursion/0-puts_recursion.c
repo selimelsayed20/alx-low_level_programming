8142c9f0a6d69ad427f8b505c17352bfa6c9728a
Selim Elsayed <saleemelsayed17@gmail.com>
2023-01-04 09:15:15 +0200

    selim

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,3 +1,20 @@
 #include "main.h"
-#include "_putchar.c"
-#include "0-main.c"
+
+/**
+ * _puts_recursion - print a string.
+ * @s: pointer to char
+ * Return: No.
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+	_putchar('\n');
+	}
+}
--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,3 +1,17 @@
 #include "main.h"
-#include "_putchar.c"
-#include "1-main.c"
+
+/**
+ * _print_rev_recursion - print a string.
+ * @s: pointer to char
+ * Return: No.
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+
+	}
+}
